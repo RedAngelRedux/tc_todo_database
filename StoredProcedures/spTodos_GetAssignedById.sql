2c8ca8fd9a578a724d3cbd3d538cd0e78ff2b64a
Sammy Nava <68456797+RedAngelRedux@users.noreply.github.com>
2024-08-26 17:46:00 -0500

    Initial Code Upload
    Initial upload of library from development machine.

---
+++ StoredProcedures/spTodos_GetAssignedById.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spTodos_GetAssignedById]
+	@AssignedTo int,
+	@TodoId int
+as
+begin
+	select Id, Task, AssignedTo, IsComplete
+	from dbo.Todos
+	where AssignedTo = @AssignedTo 
+		and Id = @TodoId;
+end
---
+++ StoredProcedures/spTodos_UpdateTask.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spTodos_UpdateTask]
+	@Task nvarchar(50),
+	@AssignedTo int,
+	@TodoId int
+as
+begin
+	update dbo.Todos
+	set Task = @Task
+	where Id = @TodoId 
+		and AssignedTo = @AssignedTo;
+end
